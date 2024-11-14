419945856e86234d3b702f30d23d8f8ddcdb29a6
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-11-14 15:28:48 -0800

    projects and contacts sections are responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,33 +4,34 @@ import me from '../pictures/me.jpg';
 function Contact() {
     return (
         <div id="contact" className='contact w-screen bg-black text-white border-t border-t-slate-300'>
-            <div className='contact-container flex flex-row items-center justify-center p-20'>
-                <div className='get-in-touch flex flex-col items-center w-1/2'>
-                    <h1 className='text-8xl font-bold pb-14 fade-in-up'>Hire me !</h1>
-                    <div className='contact-des text-xl pb-10'>
+            <div className='contact-container flex flex-col md:flex-row items-center justify-center p-6 md:p-20'>
+                <div className='get-in-touch flex flex-col items-center w-full md:w-1/2 mb-8 md:mb-0'>
+                    <h1 className='text-4xl md:text-8xl font-bold pb-8 md:pb-14 fade-in-up'>Hire me !</h1>
+                    <div className='contact-des text-base md:text-xl pb-6 md:pb-10'>
                       <a href="mailto:dnhan1707@gmail.com" title="Send me an email" className="hover:underline">dnhan1707@gmail.com</a>
                     </div>
-                    <div className='social-link mt-8'>
-                        <ul className="social-media-list flex justify-center space-x-5">
+                    <div className='social-link mt-4 md:mt-8'>
+                        <ul className="social-media-list flex justify-center space-x-4 md:space-x-5">
                             <li>
-                                <a href="https://www.linkedin.com/in/nhan-tri-danh-29b922223/" className="contact-icon text-gray-400 hover:text-white fa-2x">
-                                    <i class="fa-brands fa-linkedin"></i>
+                                <a href="https://www.linkedin.com/in/nhan-tri-danh-29b922223/" className="contact-icon text-gray-400 hover:text-white text-2xl md:fa-2x">
+                                    <i className="fa-brands fa-linkedin"></i>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://github.com/dnhan1707" className="contact-icon text-gray-400 hover:text-white fa-2x">
-                                    <i class="fa-brands fa-github"></i>                    
+                                <a href="https://github.com/dnhan1707" className="contact-icon text-gray-400 hover:text-white text-2xl md:fa-2x">
+                                    <i className="fa-brands fa-github"></i>                    
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </div>
 
-                <div className='my-image w-1/2'>
-                    <img src={me} className='fade-in-image scale-up-on-hover' alt="Nhan Tri Danh"/>
+                <div className='my-image w-full md:w-1/2 px-4 md:px-0'>
+                    <img src={me} className='fade-in-image scale-up-on-hover w-full max-w-md mx-auto' alt="Nhan Tri Danh"/>
                 </div>
             </div>
         </div>
     )
 }
+
 export default Contact; 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,15 +5,15 @@ import diagarrow from '../pictures/diag-arrow.svg';
 
 function Projects() {
     return (
-        <div className='project w-screen bg-black p-20 flex justify-between border-t border-t-slate-300 fade-in'>
-            <div className='w-1/2 pr-10'>
-                <div className='h-1/2'>
+        <div className='project w-screen bg-black p-4 md:p-20 flex flex-col md:flex-row justify-between border-t border-t-slate-300 fade-in'>
+            <div className='w-full md:w-1/2 md:pr-10 mb-10 md:mb-0'>
+                <div className='h-full'>
                     <div className='experience-item flex flex-col mb-5'>
                         <div className='image-wrapper'>
                             <div className='flex flex-row'>
-                                <img src={diagarrow} alt="Arrow"></img>
+                                <img src={diagarrow} alt="Arrow" className="w-4 md:w-auto"></img>
                                 <a 
-                                    className='text-white hover:text-blue-500 hover:underline transition duration-300' 
+                                    className='text-white text-sm md:text-base hover:text-blue-500 hover:underline transition duration-300' 
                                     href='https://github.com/dnhan1707/journey-ai-product-version'
                                 >
                                     View on Github
@@ -21,50 +21,48 @@ function Projects() {
                             </div>
 
                             <Link to="/journeyai#journeyai-intro-section">
-                                <img className='experience-image scale-up-on-hover pt-2' src={journeyAi} alt="Experience 1"></img>
+                                <img className='experience-image scale-up-on-hover pt-2 w-full' src={journeyAi} alt="Experience 1"></img>
                             </Link>
                         </div>
                         <div className='experience-3-des flex flex-row pt-5'>
-                            <div className='pl-5'>
-                                <h1 className="text-3xl text-white font-bold pt-1">Journey AI</h1>
-                                <p className='text-sm text-gray-500'>JavaScript, React, Firebase, Gemini</p>
-                                <div className='text-white pt-5'>
-                                    <h1>-{'>'}Create the most personalized travelling trip in a fast, convinient and smart way.</h1>
+                            <div className='pl-2 md:pl-5'>
+                                <h1 className="text-2xl md:text-3xl text-white font-bold pt-1">Journey AI</h1>
+                                <p className='text-xs md:text-sm text-gray-500'>JavaScript, React, Firebase, Gemini</p>
+                                <div className='text-white pt-3 md:pt-5 text-sm md:text-base'>
+                                    <h1>-{'>'}Create the most personalized travelling trip in a fast, convenient and smart way.</h1>
                                     <h1>-{'>'}JourneyAI received <span className="highlight">219 users after released</span>, and it still growing.</h1>
                                 </div>
-                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className='w-1/2 pl-10'>
-                <div className='h-1/2'>
-                    <h1 className="name text-9xl text-white font-bold pb-14 fade-in-up">Projects</h1>
+            <div className='w-full md:w-1/2 md:pl-10'>
+                <div className='h-full'>
+                    <h1 className="name text-6xl md:text-9xl text-white font-bold pb-8 md:pb-14 fade-in-up">Projects</h1>
                     <div className='project-item flex flex-col mb-5'>
-                    <   div className='flex flex-row'>
-                            <img src={diagarrow} alt="Arrow"></img>
+                        <div className='flex flex-row'>
+                            <img src={diagarrow} alt="Arrow" className="w-4 md:w-auto"></img>
                             <a 
-                                className='text-white hover:text-blue-500 hover:underline transition duration-300' 
+                                className='text-white text-sm md:text-base hover:text-blue-500 hover:underline transition duration-300' 
                                 href='https://github.com/dnhan1707/taskbreaker'
                             >
                                 View on Github
                             </a>
                         </div>
-                    <div className='image-wrapper pt-2'>
-                        <img className='experience-image scale-up-on-hover' src={taskflowAi} alt="Experience 1"></img>
-                    </div>
-                    <div className='project-1-des flex flex-row pt-5'>
-                        <div className='pl-5'>
-                        <h1 className="text-3xl text-white font-bold pt-1">TaskFlow AI <span className='text-sm text-gray-500'>{'(Cerebrals Beach Hackathon)'}</span></h1>
-                        <p className='text-sm text-gray-500'>JavaScript, React, Firebase, GPT-4o</p>
-                        <h1 className='pt-5 text-white'>-{'>'}TaskFlow AI is an innovative <span className='highlight'>task management application</span> designed to streamline project workflows and enhance team collaboration.</h1>
+                        <div className='image-wrapper pt-2'>
+                            <img className='experience-image scale-up-on-hover w-full' src={taskflowAi} alt="Experience 1"></img>
+                        </div>
+                        <div className='project-1-des flex flex-row pt-5'>
+                            <div className='pl-2 md:pl-5'>
+                                <h1 className="text-2xl md:text-3xl text-white font-bold pt-1">TaskFlow AI <span className='text-xs md:text-sm text-gray-500'>{'(Cerebrals Beach Hackathon)'}</span></h1>
+                                <p className='text-xs md:text-sm text-gray-500'>JavaScript, React, Firebase, GPT-4</p>
+                                <h1 className='pt-3 md:pt-5 text-sm md:text-base text-white'>-{'>'}TaskFlow AI is an innovative <span className='highlight'>task management application</span> designed to streamline project workflows and enhance team collaboration.</h1>
+                            </div>
                         </div>
                     </div>
-                    </div>
                 </div>
-
             </div>
         </div>
     )
