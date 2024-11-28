c17f5d4245c2a0a2081665d4014f6b37d5061fc8
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-11-27 22:50:52 -0800

    update link to resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header(){
         <div className='header w-full bg-black text-white px-32 md:px-32 pt-10'>
             <div className='header-container flex flex-row gap-4 md:gap-10 items-end justify-end'>
             <div className='resume'>
-                <a href="https://docs.google.com/document/d/1mFIFTfVFerzVRF0_SC2MblSrJ5gBSl57/edit?usp=sharing&ouid=112158056507408381785&rtpof=true&sd=true">Resume</a>
+                <a href="https://drive.google.com/file/d/1b6wfkapIkvwtaz5XmwHlFbAjgllZJWy0/view?usp=sharing">Resume</a>
             </div>
 
             <div className='contact'>
