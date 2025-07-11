1a5171abb43e3da7df0d68eb1572acf0a4dd2606
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2025-07-11 16:20:12 -0700

    update resume link

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -56,7 +56,7 @@ function Intro(){
                                 <a href="https://www.linkedin.com/in/nhan-tri-danh-29b922223/" className="hover:text-white transition-colors">
                                     LinkedIn
                                 </a>
-                                <a href="https://drive.google.com/file/d/1k1HTtntOphHTUeJ-1h7b-Xn5un7Nkwmx/view?usp=sharing" className="hover:text-white transition-colors">
+                                <a href="https://drive.google.com/file/d/1eI9KqIDR6hcOTIOJRrAQQ0t1QM-qmhXv/view?usp=sharing" className="hover:text-white transition-colors">
                                     Resume
                                 </a>
                             </div>
